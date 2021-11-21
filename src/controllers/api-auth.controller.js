e7e08119f1485893b460584673b9273994f85475
BoobyLilD <steblin73@gmail.com>
2021-11-21 14:40:58 +0300

    added controllers' structure

---
+++ src/controllers/api-auth.controller.js
@@ -0,0 +1,49 @@
+const { Router } = require("express");
+const ErrorResponse = require("../classes/error-response");
+const { nanoid } = require("nanoid");
+const User = require("../dataBase/models/User.model");
+const Token = require("../dataBase/models/Token.model");
+const { asyncHandler } = require("../middlewares/middlewares");
+
+const router = Router();
+
+function initRouters() {
+  router.post("/registration", asyncHandler(registration));
+  router.post("/login", asyncHandler(login));
+}
+
+async function registration(req, res, next) {
+  let used = await User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (used) {
+    throw new ErrorResponse("login or email already used", 400);
+  }
+  let user = await User.create(req.body);
+  res.status(200).json(user);
+}
+
+async function login(req, res, next) {
+  let user = await User.findOne({
+    attributes: ["id", "password", "login"],
+    where: {
+      login: req.body.login,
+      password: req.body.password,
+    },
+  });
+  if (!user) {
+    throw new ErrorResponse("Wrong login or password", 400);
+  }
+
+  const token = await Token.create({
+    userId: user.id,
+    value: nanoid(128),
+  });
+  res.status(200).josn({ accessToken: token.value });
+}
+
+initRouters();
+
+module.exports = router;
