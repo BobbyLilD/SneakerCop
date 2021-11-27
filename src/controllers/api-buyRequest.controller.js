b734df7a47752a07f6eb13ca95ccf3cb9097e97f
BoobyLilD <steblin73@gmail.com>
2021-11-27 13:57:41 +0300

    minor improvements

---
+++ src/controllers/api-buyRequest.controller.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const ErrorResponse = require("../classes/error-response");
+const { asyncHandler } = require("../middlewares/middlewares");
+
+const router = Router();
+
+function initRouters() {
+    
+}
+
---
+++ src/controllers/api-items.controller.js
@@ -0,0 +1,76 @@
+const { Router } = require("express");
+const ErrorResponse = require("../classes/error-response");
+const { asyncHandler } = require("../middlewares/middlewares");
+const Item = require("../database/models/Item.model");
+const User = require("../database/models/User.model");
+
+const router = Router();
+
+function initRouters() {
+  
+}
+
+async function getItems(req,res) {
+    let items = await Item.findAll();
+    res.status(200).json({items});
+}
+
+async function getItemById(req,res) {
+    let item = await Item.findByPk({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(item);
+}
+
+async function getItemsByTime(req,res){
+    let user = await User.findOne({
+        where: {
+            id: req.userId
+        }
+    })
+    if(!user){
+        throw new ErrorResponse("Something went wrong", 500);
+    }
+    let items = await Item.findAll({
+        where:{
+            publishDate: {[Op.gte]: user.lastChecked}
+        }
+    })
+    res.status(200).json(items);
+}
+
+async function updateItemById(req, res){
+    let item = await Item.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    if(!item){
+        throw new ErrorResponse("No item found", 404);
+    }
+    await item.update(req.body);
+    res.status(200).json(item);
+}
+
+async function deleteItems(req,res){
+    await Item.destroy();
+    let items = await Item.findAll();
+    res.status(200).json(items);
+}
+
+async function deleteItemById(req,res){
+    await Item.destroy({
+        where:{
+            id: req.params.id
+        }
+    })
+
+    let items = await Item.findAll();
+    res.status(200).json({items});
+}
+
+initRouters();
+
+module.exports = router;
